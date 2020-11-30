1fa3bd4e75f17dd8f50f17202c1968ee674645a7
Akira_IRL <akikun.1995@gmail.com>
2020-11-30 19:55:40 +0900

    added 3link_swing

--- flip_back_system/include/global_definitions.h
+++ flip_back_system/include/global_definitions.h
@@ -1,12 +1,12 @@
 // definition
 #define SIM_TIME            30
 #define LOOP_RATE           100
-#define SHOULDER_OFFSET     -160
+#define SHOULDER_OFFSET     -770
 #define WAIST_OFFSET_FLIP   -21
 #define WAIST_OFFSET_SWING  -25
 #define START_ANGLE         -179.5
-#define SHOULDER_FLIP_GOAL  717
-#define WAIST_FLIP_GOAL     796
+#define SHOULDER_FLIP_GOAL  742
+#define WAIST_FLIP_GOAL     740
 
 /*  動かす前にオフセット入力
     画面下のターミナルで"cm" */
---
+++ flip_back_system/src/3link_swing.cpp
@@ -0,0 +1,110 @@
+// c++ lib
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+// ros lib
+#include <ros/ros.h>
+// msg lib
+#include <std_msgs/String.h>
+#include <std_msgs/Int32.h>
+#include <std_msgs/Float64.h>
+#include <std_msgs/Int32MultiArray.h>
+#include <std_msgs/MultiArrayDimension.h>
+#include <std_msgs/MultiArrayLayout.h>
+#include <xsens_msgs/orientationEstimate.h>
+// private include header(s)
+#include "global_definitions.h"
+
+// prototype of callback/private function(s)
+void encoder_callback(const std_msgs::Float64 data);
+vector<string> split(string& input, char delimiter);
+
+// global variable(s)
+float encoder_angle = 0.0;
+
+int main(int argc, char **argv){
+    // setup as ROS node
+    ros::init(argc,argv, "flip_controller");
+    ros::NodeHandle nh;
+    // setup Publisher
+    ros::Publisher pub0 = nh.advertise<std_msgs::Int32>("program_state",1);
+    ros::Publisher pub1 = nh.advertise<std_msgs::Int32MultiArray>("dxl_target/Target_position",1);
+    // setup Subscriber
+    ros::Subscriber encoder_data  = nh.subscribe("Angle", 1, encoder_callback);
+    //setup loop rate
+    ros::Rate rate=LOOP_RATE;
+
+    // ROS messages
+    std_msgs::Int32 program_state;
+    std_msgs::Int32MultiArray dynamixel_goal;
+    dynamixel_goal.data.resize(2);
+
+    // message datum
+    program_state.data = 0;
+
+    int goal_position[2] = {SHOULDER_FLIP_GOAL,WAIST_FLIP_GOAL};
+
+    dynamixel_goal.data[0] = -goal_position[0]+SHOULDER_OFFSET;
+    dynamixel_goal.data[1] = -goal_position[1]+WAIST_OFFSET_FLIP;
+
+    // read CSV data
+    ifstream ifs("data.csv"); //set motion data source
+
+    string line;
+    vector<vector<string> > strvec;
+    int read_data;
+
+    while (getline(ifs, line)) {
+        
+        strvec.push_back(split(line, ','));
+        
+        for (int i=0; i<strvec.size();i++){
+            for(int j=0; j<strvec.at(i).size();j++){
+                istringstream iss(strvec.at(i).at(j));
+                iss >> read_data;
+                printf("%5d", read_data);
+            }
+            printf("\n");
+        }
+        printf("---\n");
+    }
+
+    ROS_INFO("Waiting for input");
+
+    do{
+        std::cin >> program_state.data;
+    }
+    while(program_state.data != 1);
+
+    while(ros::ok()){
+        ros::spinOnce();
+
+        pub0.publish(program_state);
+
+        if(encoder_angle >= START_ANGLE){
+            pub1.publish(dynamixel_goal);
+            ROS_INFO("TGT 1,2 :%d,%d",dynamixel_goal.data[0],dynamixel_goal.data[1]);
+        }
+
+        rate.sleep();
+    }
+}
+
+void encoder_callback(const std_msgs::Float64 data){
+    encoder_angle = data.data;
+    // ROS_INFO("ENC :%f",encoder_angle);
+}
+
+vector<string> split(string& input, char delimiter){
+    istringstream stream(input);
+    string field;
+    vector<string> result;
+    while (getline(stream, field, delimiter)) {
+        result.push_back(field);
+    }
+    return result;
+}
